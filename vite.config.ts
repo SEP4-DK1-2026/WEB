88949cb03759682a8c5b442769e5b11b9ac03e0a
Martin Andersen <Guffe2010@gmail.com>
2026-05-07 12:02:10 +0200

    Update vite.config.ts
    removed babel workaround as it is not used anymore

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vitest/config';
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
 
-// Workaround for typing mismatch after swapping to TypeScript
-const reactCompilerBabelOptions = {
-  presets: [reactCompilerPreset()],
-} as Parameters<typeof babel>[0]
 
 // https://vite.dev/config/
 export default defineConfig({
