c7f7a69ad112e0e06988ee96e9ecdd054d22e0b5
Rasmus Duus <342727@via.dk>
2026-05-15 09:32:02 +0000

    Initial update to new icon pack

--- src/utils/weatherIcons.ts
+++ src/utils/weatherIcons.ts
@@ -1,11 +1,10 @@
-import Sun from "../assets/sun.png"
-import Cloudy from "../assets/cloudy.png"
-import Rain from "../assets/rain.png"
-import HeavyRain from "../assets/heavy_rain.png"
-import HeavyCloudy from "../assets/heavy_cloudy.png"
-import SunAndRain from "../assets/sun_and_rain.png"
-import Snow from "../assets/snow.png"
-import Haze from "../assets/haze.png"
+import Sun from "../assets/weather-icons/animated/day.svg"
+import Cloudy from "../assets/weather-icons/animated/cloudy-day-2.svg"
+import Rain from "../assets/weather-icons/animated/rainy-4.svg"
+import HeavyRain from "../assets/weather-icons/animated/rainy-6.svg"
+import HeavyCloudy from "../assets/weather-icons/animated/cloudy.svg"
+import SunAndRain from "../assets/weather-icons/animated/rainy-1.svg"
+import Snow from "../assets/weather-icons/animated/snowy-6.svg"
 
 type WeatherIconData = {
   temperature: number
@@ -19,7 +18,7 @@ export function getWeatherIcon(data: WeatherIconData) {
   if (data.precipitation > 8) return HeavyRain
   if (data.precipitation > 2 && data.light > 15000) return SunAndRain
   if (data.precipitation > 1) return Rain
-  if (data.humidity > 85 && data.light < 5000) return Haze
+  if (data.humidity > 85 && data.light < 5000) return HeavyCloudy
   if (data.light > 25000) return Sun
   if (data.light < 8000) return HeavyCloudy
 
